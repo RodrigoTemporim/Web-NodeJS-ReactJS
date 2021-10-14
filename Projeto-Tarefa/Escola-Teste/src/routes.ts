a1e9ac1c7d7ed5e849e9a3ae59d950917d2c6bc3
Rodrigo Temporim <juniortemporim@gmail.com>
2021-10-14 20:45:13 -0300

    Revert "front-end"
    This reverts commit 5091d2259c4a70ccb3c488d7a50c188bc179f9c9.

---
+++ Projeto-Tarefa/Escola-Teste/src/routes.ts
@@ -0,0 +1,10 @@
+import { Router, request, response, Request, Response } from "express"
+
+const routes = Router()
+
+routes.get("/home", (request: Request, response: Response) => {
+    return response.json ({ message: "Hello turma!" })
+
+})
+
+export default routes
